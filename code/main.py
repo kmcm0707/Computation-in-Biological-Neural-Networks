effe7e62b096873f1369b84ef5fe4317f11b84c5
Kyle McMillan <kcm40@cam.ac.uk>
2025-02-11 06:50:32 +0000

    chore ssh

--- code/main.py
+++ code/main.py
@@ -3,5 +3,5 @@ from metalearners.runner import runner_main
 
 if __name__ == "__main__":
     # torch.autograd.set_detect_anomaly(True)
-    # main()
-    runner_main()
+    main()
+    # runner_main()
