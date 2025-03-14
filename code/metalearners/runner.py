feat: rnn additions and backprop fixes

code/metalearners/runner.py
@@ -112,8 +112,13 @@ class Runner:
         self.UpdateWeights.load_state_dict(state_dict)
         if self.options.trainFeedback:
             feedback_state_dict = torch.load(
-                self.options.modelPath + "/UpdateFeedbackWeights.pth", weights_only=True, map_location=self.device
-            )
+                self.options.modelPath + "/UpdateWeights.pth", weights_only=True, map_location=self.device
+            )  # UpdateFeedbackWeights
+            for key, val in self.UpdateWeights.named_parameters():
+                if "bias_dictionary.chemical" in key:
+                    name = "bias_dictionary.feedback_chemical" + key[-1]
+                    feedback_state_dict[name] = feedback_state_dict[key].clone()
+                    feedback_state_dict.pop(key)
             if self.options.feedbackModel == modelEnum.individual:
                 if "v_vector" in feedback_state_dict:
                     feedback_state_dict["v_dictionary.all"] = feedback_state_dict["v_vector"].clone()
@@ -318,9 +323,6 @@ class Runner:
                 self.UpdateWeights.initial_update(parameters, h_parameters)
                 if self.options.trainFeedback and self.feedbackModelOptions.operator != operatorEnum.mode_3:
                     self.UpdateFeedbackWeights.initial_update(parameters, feedback_params)
-            for key in parameters:
-                if "forward" in key:
-                    print(torch.norm(parameters[key]))
             # -- reset time index
             self.UpdateWeights.reset_time_index()
             if self.options.trainFeedback:
@@ -420,9 +422,6 @@ class Runner:
                     # -- update feedback time index
                     self.UpdateFeedbackWeights.update_time_index()
 
-            for key in parameters:
-                if "forward" in key:
-                    print(torch.norm(parameters[key]))
             """ meta test"""
             # -- predict
             y, logits = None, None
@@ -579,7 +578,7 @@ def run(
             maxTau=100,
             y_vector=yVectorEnum.none,
             z_vector=zVectorEnum.all_ones,
-            operator=operatorEnum.mode_5,
+            operator=operatorEnum.mode_6,
             train_z_vector=False,
             mode=modeEnum.all,
             v_vector=vVectorEnum.default,
@@ -698,7 +697,7 @@ def run(
         numberOfClasses=numberOfClasses,  # Number of classes in each task (5 for EMNIST, 10 for fashion MNIST)
         dataset_name=dataset_name,
         chemicalInitialization=chemicalEnum.same,
-        trainFeedback=False,
+        trainFeedback=True,
         feedbackModel=feedbackModel,
         minTrainingDataPerClass=minTrainingDataPerClass,
         maxTrainingDataPerClass=maxTrainingDataPerClass,
@@ -738,13 +737,13 @@ def runner_main():
     """
     # -- run
     # torch.autograd.set_detect_anomaly(True)
-    modelPath_s = [os.getcwd() + "/results/normalise_weights_fix/0/20250313-054852"]
+    modelPath_s = [os.getcwd() + "/results/normalise_weight_mode_6/0/20250313-054852"]
     for i in range(2):
-        for index in range(1, 27):
+        for index in range(2, 27):
             run(
                 seed=0,
                 display=True,
-                result_subdirectory=["runner_normalise_weights_fix"][i],
+                result_subdirectory=["runner_test"][i],
                 index=index,
                 typeOfFeedback=typeOfFeedbackEnum.FA,
                 modelPath=modelPath_s[i],

code/nn/backprop.py
@@ -93,10 +93,11 @@ class MetaLearner:
 
         # -- data params
         self.trainingDataPerClass = trainingDataPerClass
-        self.queryDataPerClass = 10
+        self.queryDataPerClass = 20
         self.metatrain_dataset = metatrain_dataset
         self.data_process = DataProcess(
-            trainingDataPerClass=self.trainingDataPerClass,
+            minTrainingDataPerClass=self.trainingDataPerClass,
+            maxTrainingDataPerClass=self.trainingDataPerClass,
             queryDataPerClass=self.queryDataPerClass,
             dimensionOfImage=28,
             device=self.device,
@@ -122,9 +123,7 @@ class MetaLearner:
         if self.save_results:
             self.result_directory = os.getcwd() + "/results"
             os.makedirs(self.result_directory, exist_ok=True)
-            self.result_directory += (
-                "/" + result_subdirectory + "/" + str(seed) + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-            )
+            self.result_directory += "/" + result_subdirectory + "/" + str(seed) + "/" + str(self.trainingDataPerClass)
             try:
                 os.makedirs(self.result_directory, exist_ok=False)
                 with open(self.result_directory + "/arguments.txt", "w") as f:
@@ -161,9 +160,6 @@ class MetaLearner:
         :return: model with flags , "adapt", set for its parameters
         """
         model = RosenbaumNN(self.device)
-        model.apply(self.weights_init)
-        self.UnOptimizedmodelStateDict = copy.deepcopy(model.state_dict())
-
         return model
 
     @staticmethod
@@ -203,11 +199,11 @@ class MetaLearner:
 
             # -- re initialize model
             self.model.train()
-            self.model.load_state_dict(copy.deepcopy(self.UnOptimizedmodelStateDict), strict=True)
+            self.model.apply(self.weights_init)
             self.UpdateParameters = optim.Adam(self.model.parameters(), lr=1e-3)
 
             # -- training data
-            x_trn, y_trn, x_qry, y_qry = self.data_process(data, self.number_of_classes)
+            x_trn, y_trn, x_qry, y_qry, current_training_data = self.data_process(data, self.number_of_classes)
 
             """ adaptation """
             for itr_adapt, (x, label) in enumerate(zip(x_trn, y_trn)):
@@ -220,28 +216,25 @@ class MetaLearner:
 
                 # -- backprop
                 self.UpdateParameters.zero_grad()
-                loss_meta = loss_adapt
-                loss_meta.backward()
+                loss_adapt.backward()
                 self.UpdateParameters.step()
 
-            """ meta update """
             # -- predict
             self.model.eval()
             y, logits = self.model(x_qry)
 
-            # -- compute and store meta stats
+            # -- compute and store stats
             pred = torch.argmax(logits, dim=1)
             acc = torch.eq(pred, y_qry.ravel()).sum().item() / len(y_qry.ravel())
             loss_meta = self.loss_func(logits, y_qry.ravel())
-
-            # -- gradient clipping
-            # torch.nn.utils.clip_grad_norm_(self.UpdateWeights.parameters(), 0.5)
 
             # -- log
             if self.save_results:
                 log([loss_meta.item()], self.result_directory + "/loss_meta.txt")
 
-            line = "Train Episode: {}\tLoss: {:.6f}\tAccuracy: {:.3f}".format(eps + 1, loss_meta.item(), acc)
+            line = "Train Episode: {}\tLoss: {:.6f}\tAccuracy: {:.3f}\t Current training data: {}".format(
+                eps + 1, loss_meta.item(), acc, current_training_data
+            )
             if self.display:
                 print(line)
 
@@ -255,19 +248,20 @@ class MetaLearner:
         # -- plot
         if self.save_results:
             self.summary_writer.close()
-            self.plot()
 
         # -- save
-        if self.save_results:
+        """if self.save_results:
             torch.save(
                 self.UpdateWeights.state_dict(),
                 self.result_directory + "/UpdateWeights.pth",
             )
-            torch.save(self.model.state_dict(), self.result_directory + "/model.pth")
+            torch.save(self.model.state_dict(), self.result_directory + "/model.pth")"""
         print("Meta-training complete.")
 
 
-def run(seed: int, display: bool = True, result_subdirectory: str = "testing") -> None:
+def run(
+    seed: int, display: bool = True, result_subdirectory: str = "testing", trainingDataPerClass: int = "50"
+) -> None:
     """
         Main function for Meta-learning the plasticity rule.
 
@@ -291,15 +285,20 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing") -
 
     # -- load data
     numWorkers = 6
-    epochs = 200
-    number_of_classes = 47
-    trainingDataPerClass = 90
-    dataset = EmnistDataset(trainingDataPerClass=trainingDataPerClass, queryDataPerClass=10, dimensionOfImage=28)
+    epochs = 50
+    number_of_classes = 5
+    trainingDataPerClass = trainingDataPerClass
+    dataset = EmnistDataset(
+        minTrainingDataPerClass=trainingDataPerClass,
+        maxTrainingDataPerClass=trainingDataPerClass,
+        queryDataPerClass=20,
+        dimensionOfImage=28,
+    )
     sampler = RandomSampler(data_source=dataset, replacement=True, num_samples=epochs * 5)
     metatrain_dataset = DataLoader(dataset=dataset, sampler=sampler, batch_size=number_of_classes, drop_last=True)
 
     metalearning_model = MetaLearner(
-        device="cpu",
+        device="cuda:0",
         result_subdirectory=result_subdirectory,
         save_results=True,
         metatrain_dataset=metatrain_dataset,
@@ -325,15 +324,39 @@ def backprop_main():
     :return: None
     """
     # -- run
-    run(1, True, "testing")
-    """if Args.Pool > 1:
-        with Pool(Args.Pool) as P:
-            P.starmap(run, zip([0] * Args.Pool, [False]*Args.Pool, results_directory, non_linearity, chemicals))
-            P.close()
-            P.join()
-    else:
-        run(0)"""
-
-
-def pass_through(input):
-    return input
+    trainingDataPerClass = [
+        10,
+        20,
+        30,
+        40,
+        50,
+        60,
+        70,
+        80,
+        90,
+        100,
+        110,
+        120,
+        130,
+        140,
+        150,
+        160,
+        170,
+        180,
+        190,
+        200,
+        225,
+        250,
+        275,
+        300,
+        325,
+        350,
+        375,
+    ]
+    for trainingData in trainingDataPerClass:
+        run(
+            seed=0,
+            display=True,
+            result_subdirectory="runner_backprop",
+            trainingDataPerClass=trainingData,
+        )

code/synapses/complex_synapse.py
@@ -800,7 +800,13 @@ class ComplexSynapse(nn.Module):
             update_vector[1] = -torch.matmul(activations_and_output[i + 1].T, error[i])
 
         if self.update_rules[2]:
-            update_vector[2] = -torch.matmul(error[i + 1].T, error[i])  # eHebb rule
+            update_vector[2] = -(
+                torch.matmul(error[i + 1].T, error[i])
+                - torch.matmul(
+                    parameter,
+                    torch.matmul(error[i].T, error[i]),
+                )
+            )  # eHebb rule
 
         if self.update_rules[3]:
             update_vector[3] = -parameter