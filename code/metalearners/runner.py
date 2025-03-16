c51eea7c686eb3519b3a86981ddb7483d0dbafbe
kmc0707 <kcm40@cam.ac.uk>
2025-03-16 02:19:53 +0000

    res

--- code/metalearners/runner.py
+++ code/metalearners/runner.py
@@ -536,10 +536,10 @@ def run(
     ]
     # trainingDataPerClass = [200, 225, 250, 275, 300, 325, 350, 375]
     # trainingDataPerClass = [200, 250, 300, 350, 375]
-    minTrainingDataPerClass = 500  # trainingDataPerClass[index]
-    maxTrainingDataPerClass = 500  # trainingDataPerClass[index]
+    minTrainingDataPerClass = trainingDataPerClass[index]
+    maxTrainingDataPerClass = trainingDataPerClass[index]
     queryDataPerClass = 20
-    dataset_name = "FASHION-MNIST"
+    dataset_name = "EMNIST"
 
     if dataset_name == "EMNIST":
         numberOfClasses = 5
@@ -747,7 +747,7 @@ def runner_main():
             run(
                 seed=0,
                 display=True,
-                result_subdirectory=["runner_7_chem_mode_6_800_min_tau_500"][i],
+                result_subdirectory=["runner_7_chem_mode_6_800_min_tau_500_2"][i],
                 index=index,
                 typeOfFeedback=typeOfFeedbackEnum.FA,
                 modelPath=modelPath_s[i],
