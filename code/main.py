3c438c7e89c1518b4692cf0943c177af47525228
kmc0707 <kcm40@cam.ac.uk>
2025-03-17 21:25:33 +0000

    res

--- code/main.py
+++ code/main.py
@@ -12,8 +12,8 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 if __name__ == "__main__":
     # torch.autograd.set_detect_anomaly(True)
-    main()
+    #main()
     #runner_main()
     # load_model()
     #backprop_main()
-    #rnn_backprop_main()
+    rnn_backprop_main()
