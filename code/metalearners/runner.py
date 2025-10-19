cc047a05181ccd706b3228f914f4137b4d14aed3
kmc0707 <kcm40@cam.ac.uk>
2025-10-19 23:29:59 +0100

    res

--- code/metalearners/runner.py
+++ code/metalearners/runner.py
@@ -538,7 +538,7 @@ def run(
 
     numberOfClasses = None
     # trainingDataPerClass = [90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
-    trainingDataPerClass = [
+    """trainingDataPerClass = [
         0,
         5,
         10,
@@ -568,8 +568,8 @@ def run(
         325,
         350,
         375,
-    ]
-    """trainingDataPerClass = [
+    ]"""
+    trainingDataPerClass = [
         10,
         # 20,
         # 30,
@@ -595,13 +595,13 @@ def run(
         # 1200,
         # 1250,
         # 1300,
-    ]"""
+    ]
     # trainingDataPerClass = [200, 225, 250, 275, 300, 325, 350, 375]
     # trainingDataPerClass = [200, 250, 300, 350, 375]
     minTrainingDataPerClass = trainingDataPerClass[index]
     maxTrainingDataPerClass = trainingDataPerClass[index]
     queryDataPerClass = 20
-    dataset_name = "EMNIST"
+    dataset_name = "FASHION-MNIST"
 
     if dataset_name == "EMNIST":
         numberOfClasses = 5
@@ -757,7 +757,7 @@ def run(
         modelPath=modelPath,
         results_subdir=result_subdirectory,
         seed=seed,
-        size=sizeEnum.three_layer,
+        size=sizeEnum.three_layer_wide,
         save_results=True,
         metatrain_dataset=metatrain_dataset,
         display=display,
@@ -778,7 +778,7 @@ def run(
     #   -- number of chemicals
     numberOfChemicals = numberOfChemicals
     # -- meta-traing
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = "cuda:1" if torch.cuda.is_available() else "cpu"
     # device = "cpu"
     runner = Runner(
         device=device,
@@ -812,7 +812,7 @@ def runner_main():
         # os.getcwd() + "/results/mode_6_3_chem_1/0/20250910-204609",
         # os.getcwd() + "/results/mode_6_5_chem_1/0/20250910-204750",
         # os.getcwd() + "/results/mode_6_5_chem_lr_6/0/20250715-172436"
-        # os.getcwd() + "/results/DFA_5_chem_longer/2/20251014-003536"
+        os.getcwd() + "/results/DFA_5_chem_longer/2/20251014-003536"
         # #"/results/mode_6_7_chem_1/0/20250910-222310",
         # s.getcwd() + "/results/rl_error_scalar_grad_longer_1/0/20251007-184038",
         # os.getcwd() + "/results/rl_error_scalar_grad_longer_1/0/20251007-195827",
@@ -822,8 +822,7 @@ def runner_main():
         # os.getcwd() + "/results/DFA_longer_1/0/20251008-021457"
         # os.getcwd()
         # + "/results/DFA_longer_5/0/20251008-023058"
-        os.getcwd()
-        + "/results/DFA_longer_2/0/20251008-052203"
+        #os.getcwd()+ "/results/DFA_longer_2/0/20251008-052203"
         # os.getcwd()
         # + "/results/error_5_fixed/0/20251011-194736"
         # os.getcwd() + "/results/error_1_fixed/0/20251009-194350"
@@ -841,7 +840,7 @@ def runner_main():
                     # "runner_mode_6_5_chem_scalar",
                     # "runner_scalar_fixed_3_6",
                     # "runner_scalar_5_angle_fixed",
-                    "runner_DFA_5_chem_3_layer_EMNIST_2",
+                    "runner_DFA_5_chem_3_layer_wide_FASHION-MNIST",
                 ][i],
                 index=index,
                 typeOfFeedback=typeOfFeedbackEnum.DFA_grad,
