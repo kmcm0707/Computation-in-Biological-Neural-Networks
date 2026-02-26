f25c7ac67bdfe9e3659db2bd05e183b02c019cdd
Kyle Craig McMillan <kmc0707@hotmail.com>
2026-02-26 16:52:51 +0000

    setup changes

--- code/metalearners/jax_rnn_runner.py
+++ code/metalearners/jax_rnn_runner.py
@@ -398,7 +398,7 @@ class JaxMetaLearnerRNN:
         print("Training completed.")
 
 
-def jax_runner(index: int):
+def jax_runner(index: int, index2: int):
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # second gpu
     key = jax.random.PRNGKey(42)
     # jax.config.update("jax_enable_x64", False)
@@ -438,6 +438,8 @@ def jax_runner(index: int):
         # 14000,
         # 15000,
     ]
+    if index >= len(training_data):
+        return
     epochs = 20
 
     dataset_name = "EMNIST"
@@ -516,16 +518,22 @@ def jax_runner(index: int):
     # cuda:1
     # device = "cpu"
     current_dir = os.getcwd()
-    #runner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_1/20260219-122821"
-    #runner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_2/20260219-193712"
-    #unner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_3/20260219-193948"
-    runner = current_dir + "/results_2/jax_rnn_12/20260121-024411"
-    #runner = current_dir + "/results_2/jax_rnn_DFA_3_chems/20260221-195452"
+    # runner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_1/20260219-122821"
+    # runner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_2/20260219-193712"
+    # unner = current_dir + "/results_2/jax_rnn_Low_dim_DFA_3/20260219-193948"
+    runner = [
+        current_dir + "/results_2/jax_rnn_low_dim_6/20260225-183751",
+        current_dir + "/results_2/jax_rnn_low_dim_8/20260225-200334",
+        current_dir + "/results_2/jax_rnn_low_dim_10/20260225-212923",
+        current_dir + "/results_2/jax_rnn_low_dim_20/20260226-002204",
+        current_dir + "/results_2/jax_rnn_low_dim_30/20260226-014817",
+    ][index2]
+    # runner = current_dir + "/results_2/jax_rnn_DFA_3_chems/20260221-195452"
     # -- meta-learner options
     metaLearnerOptions = JaxRnnMetaLearnerOptions(
         seed=42,
         save_results=True,
-        results_subdir="runner_jax_5_chems_12_2",
+        results_subdir="runner_jax_rnn_low_dim_{}".format([6, 8, 10, 20, 30][index2]),
         metatrain_dataset=dataset_name,
         display=True,
         metaLearningRate=None,
@@ -545,7 +553,7 @@ def jax_runner(index: int):
         recurrent_activation=JaxActivationNonLinearEnum.softplus,
         number_of_time_steps=numberOfTimeSteps,
         load_model=runner,
-        low_dim_DFA=-1,
+        low_dim_DFA=[6, 8, 10, 15, 20, 30][index2],
     )
 
     metalearning_model = JaxMetaLearnerRNN(
@@ -561,5 +569,6 @@ def jax_runner(index: int):
 
 def main_jax_runner():
 
-    for i in range(10):
-        jax_runner(i)
+    for index in range(6):
+        for i in range(10):
+            jax_runner(i, index)
