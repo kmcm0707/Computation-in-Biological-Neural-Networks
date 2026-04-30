fe537b2ed4c2bef69f64db2532d99e2ad4983d1b
K.C. McMillan <kcm40@cam.ac.uk>
2026-05-01 00:27:44 +0100

    res

--- code/metalearners/meta_learner_main.py
+++ code/metalearners/meta_learner_main.py
@@ -498,8 +498,8 @@ class MetaLearner:
                 ),
                 strict=False,
             )
-            self.UpdateWeights.z_vector = torch.nn.Parameter(current_z_vector)
-            self.UpdateWeights.y_vector = torch.nn.Parameter(current_y_vector)
+            #self.UpdateWeights.z_vector = torch.nn.Parameter(current_z_vector)
+            #self.UpdateWeights.y_vector = torch.nn.Parameter(current_y_vector)
             # self.UpdateMetaParameters.load_state_dict(
             #  torch.load(
             #      self.options.continueTraining + "/UpdateMetaParameters.pth",
@@ -1131,9 +1131,9 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
     numWorkers = 2
     epochs = 2000
 
-    dataset_name = "COMBINED"  # "EMNIST", "FASHION-MNIST", "COMBINED", "COMBINED_2"
+    dataset_name = "EMNIST"  # "EMNIST", "FASHION-MNIST", "COMBINED", "COMBINED_2"
     minTrainingDataPerClass = 5
-    maxTrainingDataPerClass = 40
+    maxTrainingDataPerClass = 80
     queryDataPerClass = 20
     dataset_1 = None
     dataset_2 = None
@@ -1247,10 +1247,10 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
             pMatrix=pMatrixEnum.first_col,
             kMatrix=kMatrixEnum.zero,
             minTau=2,  # + 1 / 50,
-            maxTau=50,
+            maxTau=700,
             y_vector=yVectorEnum.none,
             z_vector=zVectorEnum.all_ones,
-            operator=operatorEnum.mode_6,  # _pre_activation,
+            operator=operatorEnum.mode_10,  # _pre_activation,
             train_z_vector=False,
             mode=modeEnum.all,
             v_vector=vVectorEnum.default,
@@ -1357,8 +1357,8 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
 
     continue_training = (
         # current_dir + "/results_3/mode_10_scalar_11_chems_200/0/20260417-193142"
-        current_dir + "/results_3/mode_9_3_datasets_9_chems/0/20260427-125628" 
-        # +"/results_3/mode_10_extended_13_chems"
+        current_dir #+ "/results_3/mode_9_3_datasets_9_chems/0/20260427-125628" 
+        +"/results_3/mode_10_extended_13_chems"
         #"/results_3/20251111-203959"
         #+ "/results_3/mode_9_CB/5/20251112-220930"
         # +"/results_3/mode_9_3_datasets_9_chems/2/20260426-204647"
@@ -1373,7 +1373,7 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
     #  current_dir + "/results_3/mode_9_rand/0/20251105-152312"
     # )  # "/results_3/mode_9/0/20251107-172732"
     # -- meta-learner options
-    device: Literal["cpu", "cuda"] = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device: Literal["cpu", "cuda"] = "cuda:1" if torch.cuda.is_available() else "cpu"
     metaLearnerOptions = MetaLearnerOptions(
         scheduler=schedulerEnum.none,
         metaLossRegularization=0,  # L1 regularization on P and K matrices (check 1.5)
@@ -1405,7 +1405,7 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
         queryDataPerClass=queryDataPerClass,
         datasetDevice=device,
         continueTraining=continue_training,
-        typeOfFeedback=typeOfFeedbackEnum.DFA_grad,  # scalar_sign,
+        typeOfFeedback=typeOfFeedbackEnum.scalar,  # scalar_sign,
         dimOut=dimOut,
         hrm_discount=-1,
         error_control=False,
@@ -1425,7 +1425,7 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
     )
 
     # -- number of chemicals
-    numberOfChemicals = 9
+    numberOfChemicals = 13
     # -- meta-train
     metalearning_model = MetaLearner(
         device=device,
@@ -1455,4 +1455,4 @@ def main():
     # -- run
     # torch.autograd.set_detect_anomaly(True)
     for i in range(1):
-        run(seed=0, display=True, result_subdirectory="mode_9_CB_9_chems", index=i)
+        run(seed=0, display=True, result_subdirectory="mode_10_scalar_13_chems_extended", index=i)
