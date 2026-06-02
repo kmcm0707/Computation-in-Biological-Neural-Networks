0d4c4aea810a955f2c4e754d9910be95d4a6a948
K.C. McMillan <kcm40@cam.ac.uk>
2026-06-02 13:56:51 +0100

    res

--- code/metalearners/jax_rnn_runner.py
+++ code/metalearners/jax_rnn_runner.py
@@ -407,7 +407,7 @@ class JaxMetaLearnerRNN:
 
 
 def jax_runner(index: int):
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # second gpu
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # second gpu
     key = jax.random.PRNGKey(42)
     # jax.config.update("jax_enable_x64", False)
 
@@ -458,7 +458,7 @@ def jax_runner(index: int):
     minTrainingDataPerClass = training_data[index]
     maxTrainingDataPerClass = training_data[index]
     queryDataPerClass = 20
-    numberOfTimeSteps = 7
+    numberOfTimeSteps = 28
 
     if dataset_name == "EMNIST":
         numberOfClasses = 5
@@ -536,7 +536,7 @@ def jax_runner(index: int):
     metaLearnerOptions = JaxRnnMetaLearnerOptions(
         seed=42,
         save_results=True,
-        results_subdir="runner_jax_5_chem_FF_RNN_diff_7",
+        results_subdir="runner_jax_5_chem_FF_RNN_diff_28",
         metatrain_dataset=dataset_name,
         display=True,
         metaLearningRate=None,
@@ -550,7 +550,7 @@ def jax_runner(index: int):
         hidden_size=128,
         output_size=dimOut,
         biological_min_tau=1,
-        biological_max_tau=7,
+        biological_max_tau=28,
         gradient=True,
         outer_activation=JaxActivationNonLinearEnum.tanh,
         recurrent_activation=JaxActivationNonLinearEnum.softplus,
