3d8f68bbe005ffe7702fe9c5ce534fbe22f51beb
K.C. McMillan <kcm40@cam.ac.uk>
2026-06-12 18:18:04 +0100

    res

--- code/metalearners/meta_learner_main.py
+++ code/metalearners/meta_learner_main.py
@@ -1247,12 +1247,12 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
     if model == modelEnum.complex or model == modelEnum.individual:
         modelOptions = complexOptions(
             nonLinear=nonLinearEnum.tanh,
-            update_rules=[0, 1, 2, 3, 4, 6, 9],
+            update_rules=[0, 9],
             bias=False,
             pMatrix=pMatrixEnum.first_col,
             kMatrix=kMatrixEnum.zero,
             minTau=2,  # + 1 / 50,
-            maxTau=100,
+            maxTau=50,
             y_vector=yVectorEnum.none,
             z_vector=zVectorEnum.default,
             operator=operatorEnum.mode_9,  # _pre_activation,
@@ -1363,7 +1363,8 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
         # + "/results_3/mode_9_3_datasets_13_chems/2/20260427-203241" #mode_9_CB_converted_13_chems"  # "/results_3/mode_9_CB/5/20251112-001951"
         # + "/results_3/mode_10_scalar_13_chems_100/1/20260424-042527"
         # current_dir + "/results_3/mode_10_scalar_9_chems_100/1/20260423-004818"# "/results_3/mode_10_scalar_9_chems_converted/0/20260420-190254"
-        current_dir + "/results_4/mode_9_converted_9_chems"#20251124-005417" #mode_9_rand/0/20251105-152312"
+        current_dir + #"/results_4/mode_9_converted_9_chems"#20251124-005417" 
+        "/results_4/mode_9_rand/0/20251105-152312"
         #"/results_3/mode_9_scalar_10/1/20251124-005417"
         # + "/results_3/mode_9_scalar_converted_9_chems"
         # + "/results_3/mode_9_scalar_9_chems_converted/0/20260419-173857"
@@ -1395,7 +1396,7 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
             numberOfClasses_1 if dataset_name == "COMBINED" or dataset_name == "COMBINED_2" else numberOfClasses
         ),
         dataset_name=dataset_name,
-        chemicalInitialization=chemicalEnum.same,
+        chemicalInitialization=chemicalEnum.different,
         trainSeparateFeedback=False,
         feedbackSeparateModel=feedbackModel,
         trainSameFeedback=False,
@@ -1424,7 +1425,7 @@ def run(seed: int, display: bool = True, result_subdirectory: str = "testing", i
     )
 
     # -- number of chemicals
-    numberOfChemicals = 9
+    numberOfChemicals = 5
     # -- meta-train
     metalearning_model = MetaLearner(
         device=device,
@@ -1455,4 +1456,4 @@ def main():
     # -- run
     # torch.autograd.set_detect_anomaly(True)
     for true_i in range(0,17):
-        run(seed=0, display=True, result_subdirectory="mode_9_9_chem", index=true_i, index_2=1)
+        run(seed=0, display=True, result_subdirectory="mode_9_full_ablation", index=true_i, index_2=1)
