5f4dc2911a708c0a785cf6d3f3234b46f910c3a2
Kyle Craig McMillan <kmc0707@hotmail.com>
2026-08-01 23:50:28 +0100

    res

--- code/metalearners/runner.py
+++ code/metalearners/runner.py
@@ -1447,7 +1447,7 @@ def runner_main():
         # os.getcwd() + "/results_4/error_1_fixed/0/20251009-194350",
         # os.getcwd() + "/results_4/mode_9_9_chem/0/20260611-185437",
         # os.getcwd() + "/results_4/mode_9_scalar_9_chems_converted_true/0/20260420-043518"
-        os.getcwd() + "/results_4/mode_9_rand/0/20251105-152312",
+        os.getcwd() + "/results_4/DFA_5_chem_longer/2/20251014-003536" #mode_9_rand/0/20251105-152312",
         # os.getcwd() + "/results_4/20251103-214650",
         # os.getcwd() + "/results_4/mode_7_1_chem/1/20260125-202838",
         # os.getcwd() + "/results_4/CB_gating/0/20260614-171418",
